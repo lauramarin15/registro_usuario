6c55b37bc5da252fa0877f87aabf1322aca0fc17
Santiago <user@MacBook-Air.local>
2026-01-27 22:50:53 -0500

    Projecto Django:registro usuario

---
+++ usuario_project/usuarios/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre_usuario
+    
